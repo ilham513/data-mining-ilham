d0ec250d5961a51e10f7c1026f019afbf948e3dd
ilham513 <ilham513@outlook.com>
2021-11-10 09:45:55 +0700

    baru knn

--- public/knn.php
+++ public/knn.php
@@ -1,2 +1,19 @@
 <?php
-phpinfo();
+require '../vendor/autoload.php';
+use jokodm\Datamining\Klasifikasi\KNearestNeighbors;
+
+$dtraining = [	[1.2, 2.3],
+				[2.5, 4.6], 
+				[4	,	1], 
+				[5.6, 1.2], 
+				[6	, 3.5]
+			];
+			
+$labels = ["A", "A", "B", "B", "B"];
+
+$classifier = new KNearestNeighbors($k=3);
+$classifier->train($dtraining, $labels);
+
+$pred = classifier->predict([3, 2]);
+
+echo "Data testing x=3 dan y=2 <br/> Masuk kedalam Class ".$pred;
